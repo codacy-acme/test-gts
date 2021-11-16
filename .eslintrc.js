26174dbe63a923b05d18b9b5d46366ca7bf91cd7
Hélio Rocha <hjrocha@gmail.com>
2021-11-16 15:36:14 +0000

    test js format

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+var pathJoin = require('path').join;
+
+module.exports = {
+  "extends": pathJoin(__dirname, "/node_modules/gts"),
+  "rules": {
+    "@typescript-eslint/no-empty-interface": 1,
+    "node/no-unpublished-import": [
+      "error",
+      {
+        "allowModules": [
+          "fastify-plugin",
+          "app-root-path",
+          "fastify-static",
+          "aws-sdk"
+        ]
+      }
+    ],
+    "node/no-unpublished-require": [
+      "error",
+      {
+        "allowModules": ["fastify-redis-mock"]
+      }
+    ],
+    "node/no-extraneous-import": [
+      "error",
+      {
+        "allowModules": ["fastify-plugin"]
+      }
+    ],
+    "prettier/prettier": [
+      "error",
+      {
+        "endOfLine": "auto"
+      }
+    ],
+    "complexity": ["error", 10],
+    "max-len": [
+      "error",
+      { "code": 100, "ignoreStrings": true, "ignorePattern": "^import .*" }
+    ],
+    "max-depth": "error",
+    "max-lines": ["error", 500],
+    "max-lines-per-function": ["error", 110],
+    "max-nested-callbacks": "error",
+    "max-params": ["warn", 6],
+    "max-statements": ["error", 20]
+  }
+}
